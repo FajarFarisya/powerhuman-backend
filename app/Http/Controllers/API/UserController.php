0bf8ee9ea1944ea3cc4f6f1aade823dd6405731a
FajarFarisya <fajarfarisya@gmail.com>
2023-11-14 00:41:27 +0700

    feat(UserController)- login function

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -2,10 +2,50 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    //
+    public function login(Request $request){
+        try {
+            // Validate request
+            $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+            ]);
+
+            // Find User by email
+            $credentials = $request(['email', 'password']);
+            if(!Auth::attempt($credentials)){
+                return ResponseFormatter::error('Unauthorized', 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+            if(!Hash::check($request->password, $user->password)){
+                Throw new Exception('Invalid Password');
+            }
+
+            // Generate token
+            $tokenResult = $user->createToken('authToken')->plainTextToken;
+
+            // Return response
+            return ResponseFormatter::success([
+                'access_token' => $tokenResult,
+                'token_type' => 'Bearer',
+                'user' => $user,
+            ], 'Login Success');
+
+        } catch (Exception $e) {
+            return ResponseFormatter::error('Authentication Failed!');
+        }
+
+
+    }
+
 }
